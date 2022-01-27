e1d336785f0ba95ab17d0e05df7f45f58236d698
shvalev.o <os@tort.spb.ru>
2022-01-27 21:25:10 +0300

    change Vagrant fot postgresql.conf

--- 1с/1c_bckp.bat
+++ 1с/1c_bckp.bat
@@ -44,3 +44,5 @@ REM В случае удачного резервного копирования
 ECHO %DATETIME% Успешное создание резервной копии %DUMPFILE% >> backup.log
 GOTO End
 :End
+
+"C:\Program Files\1cv8\8.3.17.1851\bin\1cv8.exe" DESIGNER /S1csrv01\BUHAlekseeva /N"Администратор" /P30 /DumpIB"E:\Data\Backup\buh_%date%.dt" /Visible
