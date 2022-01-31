cdd7d908dd07c62e945f5273bd6b17a18945fd09
Ostap Shvalev <mindgam@gmail.com>
2022-01-31 19:19:08 +0300

    add node02

--- virthw/05-virt-04-docker-compose/src/terraform/variables.tf
+++ virthw/05-virt-04-docker-compose/src/terraform/variables.tf
@@ -1,13 +1,13 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gkbfd9nj8qji9ij74j"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1g1uide9uel0o2fmtl3"
 }
 
 # Заменить на ID своего образа
