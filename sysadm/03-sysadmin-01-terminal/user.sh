426da28683f28df05ceec7f9ea2ab42fa71d1320
shvalev.o <os@tort.spb.ru>
2022-02-07 20:16:52 +0300

    add bash script for create user with default pwd

--- sysadm/03-sysadmin-01-terminal/user.sh
+++ sysadm/03-sysadmin-01-terminal/user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 groupadd prog
-for user in $(cat ./users)
+for user in $(cat ./users) # list of users
 do
-  useradd -g prog -p $(openssl passwd 12348765) $user
-  chage -d 0 $user
+  useradd -g prog -p $(openssl passwd 12348765) $user #set default passwd
+  chage -d 0 $user #
   echo "$user ALL=(ALL:ALL)" ALL >> /etc/sudoers
   done
