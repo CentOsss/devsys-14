50c62f772fe44ee6e001914cacf8f3e8f8702023
shvalev.o <os@tort.spb.ru>
2022-02-22 21:52:45 +0300

    add disk y.Cloud

--- virthw/06-db-04-postgresql/terraform/node01.tf
+++ virthw/06-db-04-postgresql/terraform/node01.tf
@@ -1,13 +1,14 @@
 resource "yandex_compute_instance" "node01" {
   name                      = "node01"
   zone                      = "ru-central1-a"
-  hostname                  = "node01.db.cloud"
+  hostname                  = "node01.netology.cloud"
   allow_stopping_for_update = true
 
   resources {
-    cores  = 4
-    memory = 4
+    cores  = 8
+    memory = 8
   }
+
 
   boot_disk {
     initialize_params {
