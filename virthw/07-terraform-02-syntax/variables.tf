fd61eb58b2f331f07b0e1a24a1949b07b63269d5
Ostap Shvalev <mindgam@gmail.com>
2022-04-17 13:42:02 +0300

    task for terraform create yc_instance

---
+++ virthw/07-terraform-02-syntax/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gkbfd9nj8qji9ij74j"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g1uide9uel0o2fmtl3"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7" {
+  default = "fd8p7vi5c5bbs2s5i67s"
+}
